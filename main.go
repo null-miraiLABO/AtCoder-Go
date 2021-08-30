bf60040780f38a24722b041cb377f92b5a6c564a
null-miraiLABO <mizuki2002b@gmail.com>
2021-08-30 14:19:11 +0900

    error clear

--- main.go
+++ main.go
@@ -11,20 +11,23 @@ func main() {
 	cmd := exec.Command("ls", "BeginnersSelection/")
 	var result, err = cmd.Output()
 
-	fmt.Printf("<ファイル一覧>\n%s\n", result)
+	fmt.Printf(">ファイル一覧\n%s\n", result)
 	fmt.Println("err : ", err)
-	fmt.Println("<問題を選択して下さい。>")
+	fmt.Println(">問題を選択して下さい。")
 	fmt.Scanf("%s", &sel)
 
 	switch sel {
 	case "practiceA":
-		fmt.Println("this is practiceA")
+		fmt.Println(">this is practiceA")
 		BeginnersSelection.PracticeA()
 	case "abc086a":
-		fmt.Println("this is abc086a")
+		fmt.Println(">this is abc086a")
 		BeginnersSelection.Abc086a()
 	case "abc081a":
-		fmt.Println("this is abc086a")
+		fmt.Println(">this is abc081a")
+		BeginnersSelection.Abc081a()
+	case "abc081b":
+		fmt.Println(">this is abc081b")
 		BeginnersSelection.Abc081b()
 	}
 
