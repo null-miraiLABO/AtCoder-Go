5780f6a43e6868dfe5b4a113275fe0ab4f643b56
null-miraiLABO <mizuki2002b@gmail.com>
2021-09-08 15:23:54 +0900

    add run,exit cmd

--- main.go
+++ main.go
@@ -2,37 +2,45 @@ package main
 
 import (
 	"atCoder/BeginnersSelection"
+	"bufio"
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 )
 
 var dir_path string = "BeginnersSelection/"
+var fl bool = true
 
 func main() {
-	var sel string
 	var command, pkg_path string
 
-	fmt.Println(">ファイル一覧")
-	list_view(dir_path)
+	fmt.Println("[AtCoder code control tool with Go]")
 
-	fmt.Println(">問題を選択して下さい。")
-	fmt.Printf("name: ")
-	fmt.Scanf("%s", &sel)
-	if sel != "q" {
-		run(sel)
+	for i := 0; fl && i < 10; i++ {
+		fmt.Printf("cmd: ")
+
+		var sc = bufio.NewScanner(os.Stdin)
+		sc.Scan()
+		st := sc.Text()
+		arr := strings.Split(st, " ")
+
+		command = arr[0]
+		if len(arr) > 1 {
+			pkg_path = arr[1]
+		} else {
+			pkg_path = ""
+		}
+
+		command_run(command, pkg_path)
 	}
-
-	fmt.Printf("cmd: ")
-	fmt.Scan(&command, &pkg_path)
-	command_run(command, pkg_path)
 
 }
 
 func command_run(cmd, str string) {
 	switch cmd {
 	case "run":
-		fmt.Println("this is run")
+		run(str)
 	case "cat":
 		cat := exec.Command("cat", dir_path+str+".go")
 		var cat_result, err = cat.Output()
@@ -40,7 +48,7 @@ func command_run(cmd, str string) {
 	case "list":
 		list_view(dir_path)
 	case "exit":
-		fmt.Println("this is exit")
+		fl = false
 	default:
 		fmt.Printf("command not found: %s \n", cmd+str)
 	}
@@ -48,6 +56,7 @@ func command_run(cmd, str string) {
 }
 
 func list_view(path string) {
+	fmt.Println(">ファイル一覧")
 	cmd := exec.Command("ls", path)
 	var ls_result, err = cmd.Output()
 
@@ -64,9 +73,10 @@ func list_view(path string) {
 }
 
 func run(sel string) {
-	fmt.Printf(">this is %s \n", sel)
+	fmt.Printf(">running : %s\n", sel)
 
 	switch sel {
+	case "q":
 	case "practiceA":
 		BeginnersSelection.PracticeA()
 	case "abc086a":
